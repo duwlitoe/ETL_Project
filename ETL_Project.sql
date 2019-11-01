68aba7d16d6b7393aef723b0a4ff0c6b3de400d1
Duwli Toe <Duwli.toe@gmail.com>
2019-10-31 21:16:08 -0400

    modified folder

--- ETL_Project.sql
+++ ETL_Project.sql
@@ -1,1 +1,14 @@
-CREATE TABLE 
+CREATE TABLE us_household_income (
+State_Name TEXT,
+County TEXT,
+Mean INT
+
+);
+
+CREATE TABLE unemployment_rate (
+Year INT,
+State TEXT,
+County TEXT,
+Rate INT
+
+);
